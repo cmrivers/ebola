edfe9a1e2559ab6c3ce330156de6356cc6b01232
Daniel Chen <Maveri2k@yahoo.com>
2014-09-18 01:53:05 -0400

    moved launch instructions to readme file

---
+++ analyses/shinyCountryTimeseries/README.md
@@ -0,0 +1,7 @@
+To run this application, browse to the parent directory of where the
+ui.R and server.R scripts, open up R such that the `getwd()` is the
+parent directory. Then load shiny, by using `library(shiny)` and then
+launch the application by typing `runApp("shiny-country-timeseries")`
+
+The app requires the following R packages to be installed: RCurl,
+ggplot2, stringr, reshape2, dplyr, scales, shiny
--- analyses/shinyCountryTimeseries/ui.R
+++ analyses/shinyCountryTimeseries/ui.R
@@ -22,16 +22,6 @@ shinyUI(fluidPage(
         mainPanel(p("This graphs the cases and deaths of each country and normalizes the onset dates all to '0' so countries can be compared"),
                   "Data was all taken from Caitlin River's 'ebola' repository",
                   a('here', href = 'https://github.com/cmrivers/ebola'),
-                  br(),
-                  "To run this application, browse to the parent directory of where the ui.R and server.R scripts, open up R
-such that the ",
-                  code("getwd()"),
-                  "is the parent directory.  Then load shiny, by using ",
-                  code("library(shiny)"),
-                  "and then launch the application by typing",
-                  code('runApp("shiny-country-timeseries")'),
-
-                  p("the app requires the following R packages to be installed: RCurl, ggplot2, stringr, reshape2, dplyr, scales, shiny"),
 
                   plotOutput("plot")
                   )
