80a01254c3da26aefadbc2deaab831f6d5b7fa42
Timothée Poisot <t.poisot@gmail.com>
2014-10-06 21:25:37 +1300

    Update server.R
    This should basically solve the problem where the country colors changed when different countries where selected.

--- analyses/shinyCountryTimeseries/server.R
+++ analyses/shinyCountryTimeseries/server.R
@@ -1,4 +1,35 @@
-# server.R
+
+# ui.R
+library(shiny)
+
+shinyUI(fluidPage(
+    titlePanel("Plotting Ebola"),
+
+    tags$head(includeScript("google-analytics.js")),
+
+    sidebarLayout(
+        sidebarPanel("Interactive plot components",
+
+                     checkboxGroupInput("countries",
+                                        label = h3("Countries to display"),
+                                        choices = c("Guinea" = "Guinea",
+                                            "Liberia" = "Liberia",
+                                            "Sierra Leone" = "SierraLeone",
+                                            "Nigeria" = "Nigeria",
+                                            "Senegal" = "Senegal",
+                                            "All" = "All"),
+                                        selected = "All"),
+                     checkboxInput("log", "Plot y-axis on log scale")
+                     ),
+
+        mainPanel(p("This graphs the cases and deaths of each country and normalizes the onset dates all to '0' so countries can be compared"),
+                  "Data was all taken from Caitlin River's 'ebola' repository",
+                  a('here', href = 'https://github.com/cmrivers/ebola'),
+
+                  plotOutput("plot")
+                  )
+        )
+))# server.R
 
 library(RCurl)
 library(ggplot2)
@@ -9,6 +40,7 @@ library(dplyr)
 library(scales)
 library(shiny)
 library(foreign)
+library(RColorBrewer)
 
 url <- "https://raw.githubusercontent.com/cmrivers/ebola/master/country_timeseries.csv"
 
@@ -63,6 +95,9 @@ shinyServer(function(input, output) {
   })
 
   plot <- reactive({
+    all <- c("Guinea", "Liberia", "SierraLeone", "Nigeria", "Senegal")
+    c_colors <- brewer.pal(length(all), 'Set 1')
+    names(c_colors) <- all
     g <- ggplot(data = data_plot(),
                 aes(x = as.numeric(day), y = as.numeric(count),
                     group = place, color = place)) +
@@ -70,7 +105,8 @@ shinyServer(function(input, output) {
                             facet_grid(~ type) +
                                 scale_x_continuous(name="Days after first report") +
                                     scale_y_continuous(name="Counts") +
-                                        ggtitle("Number of observations for days after first report")
+                                        scale_colour_manual(name="Country", values=c_colors) + 
+                                          ggtitle("Number of observations for days after first report")
 
     if(!input$log){
       return(g)
