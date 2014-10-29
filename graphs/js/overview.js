329d4a144ea3dff260004e1ee800540adfe785e7
Zach <Christensen>
2014-10-28 19:22:05 -0500

    Cleaned up the JS code and got the charts fully working

--- graphs/js/overview.js
+++ graphs/js/overview.js
@@ -7,38 +7,36 @@ $(document).ready(function () {
     });
 
     d3.csv("test-update3.csv", function (error, data) {
-        var casesDeathsChart = dc.compositeChart("#casesDeathsChart");
-        var cumulative = dc.compositeChart("#cumulative");
 
-        var casesDeathsBarChart = dc.rowChart("#casesDeathsBarChart")
-        var casesDeathsPieChart = dc.pieChart("#casesDeathsPieChart");
+        //define charts
+        var casesDeathsChart = dc.compositeChart("#casesDeathsChart"),
+            cumulative = dc.compositeChart("#cumulative"),
+            mortality = dc.numberDisplay("#mortality"),
+            casesDeathsRowChart = dc.rowChart("#casesDeathsRowChart"),
+            map = dc.geoChoroplethChart("#map"),
+            datatable = dc.dataTable("#dc-data-table");
 
-        var map = dc.geoChoroplethChart("#map");
-        var datatable = dc.dataTable("#dc-data-table");
-
-        var mortality = dc.numberDisplay("#mortality");
-
-        var ndx = crossfilter(data);
-        var all = ndx.groupAll();
-
+        //data pre-processing
         var countryNameRange = [];
         var parseDate = d3.time.format("%m/%_d/%Y").parse;
 
         data.forEach(function(d) {
-        	d.Date = parseDate(d.Date);
+          d.Date = parseDate(d.Date);
 
           if ($.inArray(d.Country, countryNameRange) == -1) {
               countryNameRange.push(d.Country);
           }
         });
 
+        //crossfilter
+        var ndx = crossfilter(data);
+        var all = ndx.groupAll();
+
         var dayDimension = ndx.dimension(function (d) { return d.Date; });
         var countryDimension = ndx.dimension(function (d) { return d.Country; });
         var typeDimension = ndx.dimension(function (d) { return d.Type; });
 
-        var group = dayDimension.group();
         var typeGroup = typeDimension.group().reduceSum( function (d) {return d.Value });
-
         var casesGroup = dayDimension.group().reduceSum( function (d) {
             if (d.Type == "Cases") {
               return d.Value;
@@ -58,23 +56,6 @@ $(document).ready(function () {
         var countryGroup = countryDimension.group().reduceSum( function (d) {
             return d.Value;
         });
-        var casesCountryGroup = countryDimension.group().reduceSum( function (d) {
-            if (d.Type == "Cases") {
-              return d.Value;
-            }
-            else {
-              return 0;
-            }
-          });
-        var deathsCountryGroup = countryDimension.group().reduceSum( function(d) {
-          if (d.Type == "Deaths") {
-            return d.Value;
-          }
-          else {
-            return 0;
-          }
-        });
-
         var cumulativeCaseGroup = dayDimension.group().reduceSum( function (d) {
           if (d.Type == "Cases") {
             return d.TotalValue;
@@ -92,67 +73,31 @@ $(document).ready(function () {
           }
         })
 
+        //get the first and last day
         var minDay = dayDimension.bottom(1)[0].Date;
         var maxDay = dayDimension.top(1)[0].Date;
-
-        var caseCount = 0;
-        var deathCount = 0;
 
         var height = 198;
 
         d3.json("locations.geojson", function (countriesJSON) {
 
-        //mortality %
-        mortality
-          .valueAccessor(function(d) {
-            return deathCount / caseCount;
-          })
-          .html({
-            some:"<span style=\"color:#555555; font-size: 26px;\">Mortality: </span><br><span style=\"color:#990000; font-size: 46px;\">%number</span>",
-            none:"<span style=\"color:#555555; font-size: 26px;\">Mortality: </span><br><span style=\"color:#990000; font-size: 46px;\">Unknown</span>"
-          })
-          .formatNumber(d3.format("%"))
-          .group(typeGroup);
-
-          //case deaths pie
-          casesDeathsPieChart
-            .width(200).height(height)
-            .dimension(typeDimension)
-            .group(typeGroup)
-            .label(function (d) {
-              if (casesDeathsPieChart.hasFilter() && !casesDeathsPieChart.hasFilter(d.key)) {
-                  return d.key + " (0)";
-              }
-              else {
-                var label = d.key;
-                label += " (" + d.value + ")";
-                return label;
-              }
-            })
-            .colors(d3.scale.ordinal().range(['#555555', '#990000']));
+          //mortality %
+          mortality
+            .valueAccessor(function(d) { return typeGroup.all()[1].value / typeGroup.all()[0].value; })
+            .html({ some:"<span style=\"color:#555555; font-size: 26px;\">Mortality: </span><br><span style=\"color:#990000; font-size: 46px;\">%number</span>" })
+            .formatNumber(d3.format("%"))
+            .group(typeGroup);
 
           //case deaths row chart
-          casesDeathsBarChart
+          casesDeathsRowChart
             .width(200).height(height)
             .margins({top: 20, left: 10, right: 10, bottom: 20})
             .dimension(typeDimension)
             .group(typeGroup)
-            // .x(d3.scale.ordinal().domain(["Cases", "Deaths"]))
-            // .xUnits(dc.units.ordinal)
-            // .renderHorizontalGridLines(true)
             .ordinalColors(['#555555', '#990000'])
-            // .elasticY(true);
             .label(function (d) {
-              if (d.key == "Cases") {
-                caseCount = d.value;
-              }
-              else {
-                deathCount = d.value;
-              }
-
-
-              if (casesDeathsBarChart.hasFilter() && !casesDeathsBarChart.hasFilter(d.key)) {
-                  return d.key + " (0)";
+              if (casesDeathsRowChart.hasFilter() && !casesDeathsRowChart.hasFilter(d.key)) {
+                return d.key + " (0)";
               }
               else {
                 var label = d.key;
@@ -160,12 +105,8 @@ $(document).ready(function () {
                 return label;
               }
             })
-            .labelOffsetY(40)
-            // title sets the row text
-            .title(function (d) {
-                return d.value;
-            })
             .elasticX(true)
+            .labelOffsetY(40)
             .xAxis().ticks(4);
 
           //map
@@ -176,19 +117,14 @@ $(document).ready(function () {
             .projection(d3.geo.mercator()
               .scale((800) / Math.PI)
               .center([-25, 16]))
-            .colors(d3.scale.quantize().range(["#CCCCCC", "#BBBBBB", "#AAAAAA", "#999999", "#888888", "#777777", "#666666", "#555555", "#444444", "#333333"]))
-            .colorDomain([0, 200])
+            .colors(d3.scale.quantize().range(["#DDD","#CCC","#BBB", "#AAA", "#999", "#888", "#777", "#666", "#555", "#444", "#333", "#222"]))
+            .colorDomain([0, typeGroup.all()[0].value])
             .colorCalculator(function (d) { return d ? map.colors()(d) : '#fff'; })
             .overlayGeoJson(countriesJSON.features, "country", function (d) {
                 return d.properties.NAME;
             })
             .title(function (d) {
                return "Country: " + d.key + "\nCount: " + d.value;
-            })
-            .on("filtered", function(chart, filter){
-              console.log("Filtered chart:");
-              caseCount = 0;
-              deathCount = 0;
             });
 
 
@@ -236,24 +172,19 @@ $(document).ready(function () {
             ])
             .brushOn(false);
 
-
             //data table
             datatable
               .dimension(dayDimension)
               .group(function(d) {return d.Day;})
-              // dynamic columns creation using an array of closures
               .columns([
                   function(d) {return d.Day;},
                   function(d) {return d.Country;},
                   function(d) {return d.Type;},
-                  function(d) {return d.Value;}
+                  function(d) {return d.Value;},
+                  function(d) {return d.TotalValue;}
               ]);
 
         dc.renderAll();
-        d3.selectAll(".dc-chart").on("filtered", function(chart, filter){
-          console.log("Filtered");
-        })
-
       });
     });
 });
